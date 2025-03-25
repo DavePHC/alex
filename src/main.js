13ee8e630a74db545140ea18f3b5eda2e326b40d
DavePhc <dmitriyphc@gmail.com>
2025-03-25 18:28:56 +0300

    add mobile menu

--- src/main.js
+++ src/main.js
@@ -8,3 +8,35 @@ rangeValue.textContent = `${rangeInput.value} %`;
 rangeInput.addEventListener("input", function () {
     rangeValue.textContent = `${this.value} %`;
 });
+
+class OverlayMenu {
+    selectors = {
+      root: "[data-js-overlay-menu]",
+      dialog: "[data-js-overlay-menu-dialog]",
+      burgerButton: "[data-js-overlay-menu-burger-button]",
+    }
+    
+    stateClasses = {
+      isActive: "is-active",
+      isLocked: "is-locked",
+    }
+    
+    constructor() {
+      this.rootElement = document.querySelector(this.selectors.root);
+      this.dialogElement = this.rootElement.querySelector(this.selectors.dialog);
+      this.burgerButtonElement = this.rootElement.querySelector(this.selectors.burgerButton);
+      this.bindEvents()
+    }
+    
+    onBurgerButtonClick = () => {
+      this.burgerButtonElement.classList.toggle(this.stateClasses.isActive);
+      this.dialogElement.open = !this.dialogElement.open;
+      document.documentElement.classList.toggle(this.stateClasses.isLocked);
+    }
+    
+    bindEvents() {
+      this.burgerButtonElement.addEventListener("click", this.onBurgerButtonClick)
+    }
+}
+
+new OverlayMenu();
