458a54aae47eb85ffaa0955ed232bacbb9074c43
Дмитрий <dmitriyphc@gmail.com>
2025-03-25 14:25:10 +0300

    add range counter

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import './styles/main.scss'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
+const rangeInput = document.getElementById("rangeInput");
+const rangeValue = document.getElementById("rangeValue");
+
+rangeValue.textContent = `${rangeInput.value} %`;
+
+rangeInput.addEventListener("input", function () {
+    rangeValue.textContent = `${this.value} %`;
+});
